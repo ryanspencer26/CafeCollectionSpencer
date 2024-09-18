ac5ee18295d1298767d6c919a4e7f6215c47c95d
Ryan Spencer <ryan.spencer26@d155.org>
2024-09-18 08:11:02 -0500

    app finished

--- CafeCollectionSpencer/ViewController.swift
+++ CafeCollectionSpencer/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     var prices: [Double] = [9.99, 5.55, 3.99, 17.99, 8.88]
     var cart: [String:Int] = [:]
     var cost: Double = 0.0
+    var adminbool: Bool = false
+    
+    @IBOutlet weak var exitAdminButton: UIButton!
+    
+    @IBOutlet weak var addCartButton: UIButton!
     
     @IBOutlet weak var foodView: UITextView!
     
@@ -38,36 +43,102 @@ class ViewController: UIViewController {
         }
         
         cartView.text = "Cart:\n"
-        
         inputLabel.text = ""
         
     }
     
     @IBAction func addCart(_ sender: Any) {
         
-        for i in 0..<foods.count{
-            if foods[i] == foodField.text! {
-                if let quantity = Int(quantityField.text!) {
-                    cartView.text += " \(foodField.text!) x\(quantity),"
-                    cart[foodField.text!] = quantity
-                    cost += (prices[i] * Double(quantity))
-                    cost *= 100.0
-                    cost = cost.rounded()
-                    cost /= 100.0
-                    inputLabel.textColor = UIColor.green
-                    inputLabel.text = "Added to cart"
-                    break
-                } else {
+        if !adminbool {
+            for (key, _) in cart {
+                if key == foodField.text!{
                     inputLabel.textColor = UIColor.red
-                    inputLabel.text = "Invalid quantity"
+                    inputLabel.text = "Already in cart"
+                    return
+                }
+            }
+            
+            for i in 0..<foods.count{
+                if foods[i] == foodField.text! {
+                    if let quantity = Int(quantityField.text!) {
+                        cartView.text += " \(foodField.text!) x\(quantity),"
+                        cart[foodField.text!] = quantity
+                        cost += (prices[i] * Double(quantity))
+                        cost *= 100.0
+                        cost = cost.rounded()
+                        cost /= 100.0
+                        inputLabel.textColor = UIColor.green
+                        inputLabel.text = "Added to cart"
+                        costView.text = "Cost:\n$\(cost)"
+                        return
+                    } else {
+                        inputLabel.textColor = UIColor.red
+                        inputLabel.text = "Invalid quantity"
+                        return
+                    }
+                }
+            }
+            inputLabel.textColor = UIColor.red
+            inputLabel.text = "Invalid food item"
+            
+        } else {
+            for i in 0..<foods.count {
+                if foodField.text! == foods[i] {
+                    foods.remove(at: i)
+                    prices.remove(at: i)
+                    inputLabel.textColor = UIColor.green
+                    inputLabel.text = "Item removed"
+                    foodView.text = ""
+                    priceView.text = ""
+                    for i in 0..<foods.count{
+                        foodView.text += "\(foods[i])\n"
+                        priceView.text += "\(prices[i])\n"
+                    }
+                    return
+                }
+            }
+            if let quantity = Double(quantityField.text!) {
+                if foodField.text! != "" {
+                    prices.append(quantity)
+                    foods.append(foodField.text!)
+                    inputLabel.textColor = UIColor.green
+                    inputLabel.text = "Item added"
+                    foodView.text = ""
+                    priceView.text = ""
+                    for i in 0..<foods.count{
+                        foodView.text += "\(foods[i])\n"
+                        priceView.text += "\(prices[i])\n"
+                    }
                 }
             } else {
                 inputLabel.textColor = UIColor.red
-                inputLabel.text = "Invalid food item"
+                inputLabel.text = "Invalid price"
             }
         }
         
-        costView.text = "Cost:\n$\(cost)"
+    }
+    
+    @IBAction func admin(_ sender: Any) {
+        
+        if(foodField.text! == "Password" || quantityField.text == "Password"){
+            inputLabel.text = ""
+            exitAdminButton.isHidden = false
+            adminbool = true
+            addCartButton.setTitle("Add/remove Item", for: .normal)
+            quantityField.placeholder = "Insert price"
+        } else {
+            inputLabel.text = "Type password"
+        }
+        
+    }
+    
+    @IBAction func exitAdmin(_ sender: Any) {
+        
+        exitAdminButton.isHidden = true
+        adminbool = false
+        addCartButton.setTitle("Add to Cart", for: .normal)
+        inputLabel.text = ""
+        quantityField.placeholder = "Insert quantity"
         
     }
     
